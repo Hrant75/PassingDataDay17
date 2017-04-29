8be5fb838f850caaadf26894585ff4af765b4858
Hrant75 <hhro@yahoo.com>
2017-04-29 12:56:25 +0400

    day17 passing data

--- app/src/main/java/com/example/hrant/passingdata/MainActivity.java
+++ app/src/main/java/com/example/hrant/passingdata/MainActivity.java
@@ -1,40 +1,46 @@
 package com.example.hrant.passingdata;
 
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
-    TextView textView;
-    String idStr;
-    int id;
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        int viewGroupId, nameLastnameId, genderId, picId;
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         for(User user : UserList.getUsersList()){
-            idStr = "view_group_" + user.getId();
-            id = getResources().getIdentifier(idStr, "id", getApplicationContext().getPackageName());
-            Log.e("sss", idStr + id);
-            if(user.getGender().equals("F"))
-                findViewById(id).setBackgroundColor(getResources().getColor(R.color.colorAccent));
-            else
-                findViewById(id).setBackgroundColor(getResources().getColor(R.color.colorPrimaryDark));
+            viewGroupId = getResources().getIdentifier("view_group_" + user.getId(), "id", getApplicationContext().getPackageName());
+            picId = getResources().getIdentifier("vg" + user.getId() + "_pic", "id", getApplicationContext().getPackageName());
 
-            idStr = "vg" + user.getId() + "_name_lastname";
-            id = getResources().getIdentifier(idStr, "id", getApplicationContext().getPackageName());
-            textView =  ((TextView)findViewById(id));
-            textView.setText(user.getName() + " " + user.getLastName());
+            if(user.getGender().equals("F")) {
+                findViewById(viewGroupId).setBackgroundColor(getResources().getColor(R.color.colorAccent));
+                ((ImageView)findViewById(picId)).setBackgroundResource(R.drawable.f);
+            }
+            else {
+                findViewById(viewGroupId).setBackgroundColor(getResources().getColor(R.color.colorPrimaryDark));
+                ((ImageView)findViewById(picId)).setBackgroundResource(R.drawable.m);
+            }
 
-            idStr = "vg" + user.getId() + "_gender";
-            id = getResources().getIdentifier(idStr, "id", getApplicationContext().getPackageName());
-            textView =  ((TextView)findViewById(id));
-            textView.setText(user.getGender());
+            nameLastnameId = getResources().getIdentifier("vg" + user.getId() + "_name_lastname", "id", getApplicationContext().getPackageName());
+            ((TextView)findViewById(nameLastnameId)).setText(user.getName() + " " + user.getLastName());
+
+            genderId = getResources().getIdentifier("vg" + user.getId() + "_gender", "id", getApplicationContext().getPackageName());
+            ((TextView)findViewById(genderId)).setText(user.getGender());
         }
+    }
+
+    @Override
+    public void onClick(View v) {
 
     }
 }
